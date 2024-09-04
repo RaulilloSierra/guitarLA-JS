6dbee9ab382d381973d7a905a56a31b6d26c58a8
RaulSierradev <illosierra@outlook.com>
2024-09-04 15:44:29 -0500

    Guitar.jsx: ajuste en la descripción

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 
 export default function Guitar({guitar}) {
-  const { id, name, image, description, price } = guitar
+  const { name, image, description, price } = guitar
   return (
     <Fragment>
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -14,8 +14,8 @@ export default function Guitar({guitar}) {
         </div>
         <div className="col-8">
           <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
-          <p>
-            {description}t
+          <p className="row-3">
+            {description}
           </p>
           <p className="fw-black text-primary fs-3">${price}</p>
           <button type="button" className="btn btn-dark w-100">
